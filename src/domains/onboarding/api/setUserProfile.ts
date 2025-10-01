35bcdbad118e8c440541baebd53cb917434b8ebb
yoon5450 <dbseodnd356@naver.com>
2025-10-01 16:25:17 +0900

    Feat: 온보딩 유저 프로필 설정 api 연결

--- src/domains/onboarding/api/setUserProfile.ts
+++ src/domains/onboarding/api/setUserProfile.ts
@@ -1,12 +1,18 @@
-import { UserOnboardingData } from "../types";
+"use client"
 
+import axios from "axios";
+import { UserOnboardingData } from "../types";
 
 export async function setUserProfile(data: UserOnboardingData) {
   const username = data.name;
   const mbti = data.mbti;
   const beliefs = data.beliefs;
   const gender = data.gender;
+
+  // additional 데이터 추출
   const additional_rawData = data.additional;
+
+  // additional 데이터 flat하게 변환
   const lifeSatis = additional_rawData.life_satis;
   const relationship = additional_rawData.relationships;
   const workLifeBal = additional_rawData.work_life_bal;
@@ -18,7 +24,7 @@ export async function setUserProfile(data: UserOnboardingData) {
     month: data.birthday_at?.birthMonth?.toString() ?? "",
     day: data.birthday_at?.birthDay?.toString() ?? "",
   };
-  
+
   // ISO 8601 형식으로 변환
   const birthdayAt = `${
     birthday_rawData.year
@@ -27,23 +33,25 @@ export async function setUserProfile(data: UserOnboardingData) {
     "0"
   )}T00:00:00`;
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/users-info`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
+  const response = await axios.post(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/v1/users-info`,
+    {
       username,
+      birthdayAt,
+      gender,
       mbti,
       beliefs,
-      additional_rawData,
-      birthdayAt,
-    }),
-  });
+      lifeSatis,
+      relationship,
+      workLifeBal,
+      riskAvoid,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
-  if (!res.ok) {
-    throw new Error(`API 요청 실패: ${res.status}`);
-  }
-
-  return res.json();
+  return response.data;
 }
