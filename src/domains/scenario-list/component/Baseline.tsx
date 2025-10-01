c6a9409b638cfd173f891536842a36af581e28aa
yoon00 <y00ndlek@gmail.com>
2025-10-01 15:08:53 +0900

    [FEAT]: 베이스라인 리스트 페이지  구현

---
+++ src/domains/scenario-list/component/Baseline.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import { Baseline as BaselineType } from "../types";
+
+interface BaseLineItemProps {
+  baseline: BaselineType;
+}
+
+function Baseline({ baseline }: BaseLineItemProps) {
+  return (
+    <div className="border border-gray-100 rounded-lg p-8 flex flex-col gap-6">
+      {/* 상단 */}
+      <div className="flex justify-between items-start">
+        <h3 className="text-xl font-medium text-gray-800">{baseline.title}</h3>
+        <div className="flex gap-2">
+          <Link
+            href={`/multiverse/${baseline.baselineId}`}
+            className="flex h-9 px-5 py-3 justify-center items-center gap-2.5 flex-shrink-0 rounded-md bg-deep-navy text-white hover:bg-opacity-90 transition"
+          >
+            평행우주 조회
+          </Link>
+          <button className="flex h-9 px-5 py-3 justify-center items-center gap-2.5 flex-shrink-0 rounded-md border border-deep-navy bg-transparent text-deep-navy hover:bg-deep-navy hover:text-white transition">
+            삭제
+          </button>
+        </div>
+      </div>
+
+      {/* 하단 */}
+      <div className="flex justify-between items-center">
+        <div className="flex flex-wrap gap-2">
+          {baseline.tags.map((tag, index) => (
+            <span
+              key={index}
+              className="text-sm bg-dusty-blue text-white px-4 py-2 rounded-full"
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+        <span className="text-base text-gray-300">{baseline.createdDate}</span>
+      </div>
+    </div>
+  );
+}
+
+export default Baseline;
